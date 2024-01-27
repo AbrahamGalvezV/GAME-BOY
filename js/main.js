edc103cd8107d0048233b6b71ac0c16968e2eff6
Abraham Galvez <154621378+AbrahamGalvezV@users.noreply.github.com>
2024-01-27 10:11:45 +0100

    añadido audio

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let botonEncendido = document.getElementById('start');
 let botonApagado = document.getElementById('off'); 
 let pantallaOff = document.getElementById('screenOff'); 
 let pantallaOn = document.getElementById('screenOn');
+let audio = new Audio();
 
 
 botonEncendido.addEventListener('click', function() {
@@ -9,6 +10,8 @@ botonEncendido.addEventListener('click', function() {
     pantallaOn.classList.add('screenOn');
     pantallaOn.classList.add('backgroundInicio');
     img.classList.add('opacity0');
+    audio.src="../audio/music_gb-start.mp3";
+    audio.play();
 });
 
 botonApagado.addEventListener('click', function() {
